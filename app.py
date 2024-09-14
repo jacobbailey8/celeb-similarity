f31e3da3b42befb05b142d5f3314d251f8aad070
jacobbailey8 <jbaileydsi87@gmail.com>
2024-09-14 01:33:52 -0400

    Made design better and dynamically can change number of celebs shown

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ import torchvision.transforms as transforms
 import psycopg2
 import boto3
 from scipy.spatial.distance import cosine
-import os
+import base64
+from io import BytesIO
 
 # Database and S3 settings
 RDS_HOST = st.secrets["RDS_HOST"]
@@ -19,6 +20,7 @@ RDS_PASSWORD = st.secrets["RDS_PASSWORD"]
 S3_BUCKET = st.secrets["S3_BUCKET"]
 AWS_ACCESS_KEY = st.secrets["AWS_ACCESS_KEY"]
 AWS_SECRET_KEY = st.secrets["AWS_SECRET_KEY"]
+
 
 # Load the pretrained FaceNet model
 facenet_model = InceptionResnetV1(pretrained='vggface2').eval()
@@ -76,6 +78,16 @@ def download_image_from_s3(image_id):
     except s3.exceptions.NoSuchKey:
         st.error(f"Error: The specified key does not exist in S3: {image_key}")
         return None
+
+
+def image_to_base64(image):
+    """
+    Convert a PIL image to a base64-encoded string.
+    """
+    buffered = BytesIO()
+    image.save(buffered, format="JPEG")
+    img_str = base64.b64encode(buffered.getvalue()).decode()
+    return img_str
 
 # Function to calculate cosine similarity
 
@@ -150,8 +162,23 @@ def getCelebName(id):
 
 
 def main():
-    st.title("Face Similarity App")
-    st.write("Upload an image or take a photo with your camera")
+    st.title("Celebrity Lookalike Detector")
+    st.write("Upload an image below to find your top lookalikes!")
+
+    # Dropdown to select the number of celebrities to display
+    num_celebs_to_display = st.selectbox(
+        "Select number of celebrities to display", [3, 5, 10], index=1
+    )
+
+    # Initialize session state to track if the processed image should be shown
+    if 'show_processed_image' not in st.session_state:
+        st.session_state['show_processed_image'] = False
+
+    # Initialize session state to store embeddings and matches
+    if 'embedding' not in st.session_state:
+        st.session_state['embedding'] = None
+    if 'top_matches' not in st.session_state:
+        st.session_state['top_matches'] = None
 
     # Image upload or camera input
     image_file = st.file_uploader(
@@ -160,46 +187,139 @@ def main():
         image_file = st.camera_input("Take a photo")
 
     if image_file is not None:
-        # Open the image
-        image = Image.open(image_file)
+        # Reset session state when a new image is uploaded
+        if 'last_uploaded_file' not in st.session_state or st.session_state['last_uploaded_file'] != image_file:
+            st.session_state['last_uploaded_file'] = image_file
+            st.session_state['embedding'] = None
+            st.session_state['top_matches'] = None
 
-        # Preprocess the image (detect and crop face)
-        image_tensor, processed_image = preprocess_image(image)
+        with st.spinner("Processing Image..."):
+
+            # Open the image
+            image = Image.open(image_file)
+
+            # Preprocess the image (detect and crop face)
+            image_tensor, processed_image = preprocess_image(image)
 
         if processed_image is not None:
-            # Display the processed image
-            st.image(processed_image,
-                     caption="Processed Face (Moved Up)", use_column_width=True)
+            # Button to show/hide the processed image
+            if st.button("Show/Hide Processed Image"):
+                # Toggle the state
+                st.session_state['show_processed_image'] = not st.session_state['show_processed_image']
 
-            # Vectorize the image
-            embedding = vectorize_image(image_tensor).numpy()
+            # Conditionally display the processed image based on session state
+            if st.session_state['show_processed_image']:
+                st.image(processed_image,
+                         caption="Processed Face (Moved Up)", use_column_width=True)
 
-            # Fetch existing vectors from the database
-            vectors = fetch_vectors_from_db()
+            # Perform similarity search only if it hasn't been done yet for the current image
+            if st.session_state['embedding'] is None:
+                with st.spinner("Finding Matches..."):
 
-            # Calculate similarities and find the top 3 matches
-            similarities = []
-            for row in vectors:
-                id, vector, _ = row
-                vector = np.array(vector)
-                similarity = cosine_similarity(
-                    embedding.flatten(), vector)
-                similarities.append((id, similarity))
+                    # Vectorize the image
+                    embedding = vectorize_image(image_tensor).numpy()
 
-            # Sort by similarity and get top 3
-            top_matches = sorted(
-                similarities, key=lambda x: x[1], reverse=True)[:3]
+                    # Fetch existing vectors from the database
+                    vectors = fetch_vectors_from_db()
 
-            # Display the top 3 matches with similarity scores
-            st.write("Top 3 matches:")
-            for match in top_matches:
-                id, similarity = match
-                name = getCelebName(id)
-                similar_image = download_image_from_s3(id)
-                if similar_image:
-                    normalized_similarity = similarity * 100  # Convert to percentage
-                    st.image(
-                        similar_image, caption=f"{name}, Similarity: {normalized_similarity:.2f}%", use_column_width=True)
+                    # Calculate similarities and find the top 3 matches
+                    similarities = []
+                    for row in vectors:
+                        id, vector, _ = row
+                        vector = np.array(vector)
+                        similarity = cosine_similarity(
+                            embedding.flatten(), vector)
+                        similarities.append((id, similarity))
+
+                    # Sort by similarity and get top 3
+                    top_matches = sorted(
+                        similarities, key=lambda x: x[1], reverse=True)[:10]
+
+                    # Store the embedding and matches in session state
+                    st.session_state['embedding'] = embedding
+                    st.session_state['top_matches'] = top_matches
+
+            # Display the top 3 matches with similarity scores if they exist
+            if st.session_state['top_matches'] is not None:
+
+                # Create a scrollable horizontal container
+                st.markdown(
+                    """
+                    <style>
+                    .scrolling-wrapper {
+                        display: flex;
+                        flex-wrap: nowrap;
+                        overflow-x: auto;
+                        white-space: nowrap;
+                    }
+                    .scrolling-wrapper div {
+                        margin-right: 20px;
+                        text-align: center;
+                    }
+                    .scrolling-wrapper img {
+                        height: 150px;
+                        width: auto;
+                    }
+                    .scrolling-wrapper p {
+                        font-size: 18px;
+                        font-weight: bold;
+                        margin: 0;
+                    }
+                    .result {
+                        text-align: center;
+                    }
+                    .name {
+                        font-size: 1.5rem;
+                        margin-bottom: 0;
+
+                    }
+                    .green {
+                        color: green;
+                        font-size: 1.5rem;
+                    }
+                    .yellow {
+                        color: yellow;
+                        font-size: 1.5rem;
+                    }
+                    .red {
+                        color: red;
+                        font-size: 1.5rem;
+                    }
+                    </style>
+                    <h1>Top Matches:</h1>
+                    <div class="scrolling-wrapper">
+                    """, unsafe_allow_html=True)
+
+                for match in st.session_state['top_matches'][:num_celebs_to_display]:
+                    id, similarity = match
+                    name = getCelebName(id)
+                    similar_image = download_image_from_s3(id)
+                    if similar_image:
+                        # Convert the PIL image to a base64 string
+                        img_base64 = image_to_base64(similar_image)
+                        normalized_similarity = similarity * 100  # Convert to percentage
+
+                        # Determine the color based on similarity
+                        if normalized_similarity > 70:
+                            similarity_color = "green"
+                        elif 40 <= normalized_similarity <= 70:
+                            similarity_color = "yellow"
+                        else:
+                            similarity_color = "red"
+
+                        # Render image inside the scrolling container
+                        st.markdown(
+                            f"""
+                            <div class="result">
+                            <img src="data:image/jpeg;base64,{img_base64}" alt="{name}"/>
+                            <p class="name">{name}</p>
+                            <p class="{similarity_color}">{normalized_similarity:.2f}%</p>
+                            </div>
+                            """, unsafe_allow_html=True)
+
+                # Close the scrollable container
+                st.markdown("</div>", unsafe_allow_html=True)
+
         else:
             st.write("No face detected. Please upload a different image.")
 
